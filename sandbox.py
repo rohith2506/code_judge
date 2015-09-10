8484a652f3daf38de01c3117fa9fc75fd81b506d
rohith.uppala <rohith.uppala@goibibo.com>
2015-09-10 23:03:51 +0530

    more changes

--- sandbox.py
+++ sandbox.py
@@ -15,59 +15,90 @@ def copy_sandbox_executable_to_prog_folder(prog_folder):
     command = "cp sandbox/sandbox " + prog_folder
     os.system(command)
 
+def get_results(result):
+    if 'TLE Time Limit exceeded (H)' in result:
+        result = "TIME LIMIT EXCEEDED"
+    elif 'MemoryError' in result:
+        result = "MEMORY LIMIT EXCEEDED"
+    elif 'RuntimeError' in result:
+        result = "RUNTIME ERROR"
+    else:
+        result = result
+    return result
 
-def execute_code(prog_folder, prog_lang, prog_file, input_file, output_file, error_file, executable):
+
+def execute_code_for_python(prog_folder, prog_lang, prog_file, input_file, output_file, error_file):
     status = True
     copy_sandbox_executable_to_prog_folder(prog_folder)
+    command = "chmod +x " + prog_folder
+    os.system(command)
     open(error_file, "w").close()
 
-    if prog_lang == "python":
-        exec_command = "#!/usr/bin/python"
-        python_file = open(prog_file, "r").read()
-        modified_python_file = exec_command + "\n" + python_file
-        outfile = open(prog_file, "w")
-        outfile.write(modified_python_file)
-        outfile.close()
-        command = "chmod +x " + prog_file
-        os.sytem(command)
-        sandbox_command = "./sandbox ./" + prog_file + " --input=" + input_file + " --output=" + \
-                          output_file + " --time=" + TIME_LIMIT + "--chroot=" + prog_folder + " --debug > " \
-                          + error_file + " 2>&1"
+    exec_command = "#!/usr/bin/python"
+    python_file = open(prog_file, "r").read()
+    modified_python_file = exec_command + "\n" + python_file
+    
+    outfile = open(prog_file, "w")
+    outfile.write(modified_python_file)
+    outfile.close()
+    command = "chmod +x " + prog_file
+    os.system(command)
+    sandbox_command = "./" + prog_folder + "/sandbox ./" + prog_file + " --input=" + input_file + \
+                      " --output=" + output_file + " --time=" + str(TIME_LIMIT) + " --chroot="  + \
+                      prog_folder + " --debug > " + error_file + " 2>&1"
+    os.system(sandbox_command)
+    
+    error_string = open(error_file, "r").read()
+    if len(error_string) > 0:
+        status = False
+        result = get_results(error_string)
+    else:
+        result = None
+        status = True
+    return (result, status)
+            
+
+def execute_code_for_compiled_progs(prog_folder, prog_lang, prog_file, input_file, output_file, error_file, executable):
+    status = True
+    copy_sandbox_executable_to_prog_folder(prog_folder)
+    command = "chmod +x " + prog_folder
+    os.system(command)
+    open(error_file, "w").close()
+
+    if prog_lang == "cpp":
+        command = "g++ " + prog_file + " -o " + executable + " > " + error_file + " 2>&1 "
         os.system(command)
         error_string = open(error_file, "r").read()
         if len(error_string) > 0:
             status = False
-    elif prog_lang == "cpp":
-        command = "g++ " + prog_file + " -o " + executable + " 2> " + error_file
-        os.system(command)
-        error_string = open(error_file, "r").read()
-        if len(error_string) > 0:
-            status = False
+            return (error_string, status)
         else:
             sandbox_command =  "./sandbox ./" + executable + " --input=" + input_file + \
                                " --output=" + output_file + " --time=" + TIME_LIMIT + \
-                                "chroot=" + prog_folder + " --debug > " + error_file + " 2>&1"
+                                "chroot=" + prog_folder + " > " + error_file + " 2>&1"
             os.system(sandbox_command)
     else:
-        command = "gcc " + prog_file + " -o " + executable + " 2> " + error_file
-        print command
+        command = "gcc " + prog_file + " -o " + executable + " > " + error_file + " 2>&1"
         os.system(command)
         error_string = open(error_file, "r").read()
         if len(error_string) > 0:
             status = False
+            return (error_string, status)
         else:
             sandbox_command =  "./sandbox ./" + executable + " --input=" + input_file + \
                                " --output=" + output_file + " --time=" + TIME_LIMIT + \
-                               " --chroot=" + prog_folder + " --debug > " + error_file + " 2>&1"
+                               " --chroot=" + prog_folder + " > " + error_file + " 2>&1"
             os.system(sandbox_command)
 
-    result = ""
-    if status == False:
-        output_file = error_file
-    for line in open(output_file, "r"):
-        result += line
-    return (result, status)
-
+    error_string = open(error_file, "r").read()
+    if len(error_string) > 0:
+        status = False
+        result = get_results(error_string)
+    else:
+        status = True
+        result = None
+    return (results, status)
+        
 
 def get_required_output(prog_id, prog_lang, code, prog_input):
     # create folder using unique id and store files
@@ -92,14 +123,17 @@ def get_required_output(prog_id, prog_lang, code, prog_input):
     ofile.write(prog_input)
     ofile.close()  
 
-    result, status = execute_code(prog_folder, prog_lang, prog_file, input_file, \
-                                              output_file, error_file, executable)
-    if 'TLE Time Limit exceeded (H)' in result:
-        result = "TIME LIMIT EXCEEDED"
-    elif 'MemoryError' in result:
-        result = "MEMORY LIMIT EXCEEDED"
-    elif 'RuntimeError' in result:
-        result = "RUNTIME ERROR"
+    if prog_lang == "python":
+        result, status = execute_code_for_python(prog_folder, prog_lang, prog_file, input_file, \
+                                                                        output_file, error_file)
     else:
-        result = result
-    return (result, status)
+        result, status = execute_code_for_compiled_progs(prog_folder, prog_lang, prog_file, \
+                                            input_file, output_file, error_file, executable)
+
+    if not result:
+        # Without any error here.
+        result = open(output_file, "r").read()
+        return (result, status)
+    else:
+        # with compile (or) runtime errors
+        return (result, status)
